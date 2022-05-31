03c3f807d4459724ef505a265156493a545272c4
Esteban <eaplata20@gmail.com>
2022-05-30 19:10:15 -0500

    se ha instalado paquete cors que nos permite el acceso y quedo configurado de cualquier origen

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 const api = require('./routes/metodos')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())/* parseamos el objeto como json */
 app.use(api)
+app.use(cors())/* cualquier origen puede consumir o hacer uso de esta api */
 
 
 module.exports = app;
