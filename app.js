3541afe8a02f707468d30ab84f454b24aaf37ae7
Esteban <eaplata20@gmail.com>
2022-05-31 23:07:56 -0500

    cors

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const cors = require('cors')
 const app = express();
 const api = require('./routes/metodos')
 
+
+app.use(cors())/* cualquier origen puede consumir o hacer uso de esta api */
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())/* parseamos el objeto como json */
 app.use(api)
-app.use(cors())/* cualquier origen puede consumir o hacer uso de esta api */
+
 
 
 module.exports = app;
